17b36be00aaa7be21fd021cf0a4ab0c2ac3cd771
Damjan Savkovic <damjans92@gmail.com>
2025-09-16 18:07:24 +0200

    breadcrumbs remove '-' character

--- src/app/components/Breadcrumbs.tsx
+++ src/app/components/Breadcrumbs.tsx
@@ -7,7 +7,6 @@ const Breadcrumbs = () => {
   const pathname = usePathname();
 
   const segments = pathname.split("/").filter((seg) => seg);
-  console.log(segments);
   return (
     <nav className="text-sm font-light text-gray-600 uppercase mt-4 ml-4">
       <ol className="flex items-center space-x-2">
@@ -21,7 +20,11 @@ const Breadcrumbs = () => {
 
         {segments.map((segment, idx) => {
           const href = "/" + segments.slice(0, idx + 1).join("/");
-          const label = segment.charAt(0).toUpperCase() + segment.slice(1);
+          console.log("segment", segment);
+
+          const label = segment.includes("-")
+            ? segment.replace(/-/g, " ")
+            : segment;
 
           return (
             <li key={idx} className="flex items-center">

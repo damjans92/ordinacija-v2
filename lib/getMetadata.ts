e464981d32bd624ab5bab0bd711abb99444713c1
Damjan Savkovic <damjans92@gmail.com>
2025-10-10 19:19:21 +0200

    getmetadata build error type fix

--- lib/getMetadata.ts
+++ lib/getMetadata.ts
@@ -2,8 +2,14 @@ import type { Metadata } from "next";
 import { seoData } from "@/data/seoData";
 
 export function getMetadata(lang: "sr" | "en", path: string): Metadata {
-  const getNested = (obj: any, keyPath: string) =>
-    keyPath.split(".").reduce((acc, key) => acc?.[key], obj);
+  const getNested = (obj: unknown, keyPath: string) =>
+    keyPath
+      .split(".")
+      .reduce(
+        (acc, key) =>
+          acc && typeof acc === "object" ? (acc as any)[key] : undefined,
+        obj
+      );
 
   const data = getNested(seoData[lang], path);
 
@@ -16,8 +22,21 @@ export function getMetadata(lang: "sr" | "en", path: string): Metadata {
     };
   }
 
-  return {
-    title: `${data.title}`,
-    description: data.description,
-  };
+  if (
+    typeof data === "object" &&
+    data !== null &&
+    "title" in data &&
+    "description" in data
+  ) {
+    return {
+      title: `${(data as { title: string }).title}`,
+      description: (data as { description: string }).description,
+    };
+  } else {
+    return {
+      title: "Dr Savković Dental Office",
+      description:
+        "Stomatolog u Beogradu – zdrav osmeh za ceo život. Ordinacija Dr Savković pruža vrhunsku stomatološku negu.",
+    };
+  }
 }

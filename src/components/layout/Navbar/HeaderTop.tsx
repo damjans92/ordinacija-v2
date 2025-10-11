77adcf855ec529e923a6784a7d57337a9d3e9b21
Damjan Savkovic <damjans92@gmail.com>
2025-10-12 00:37:42 +0200

    header top height, lang selector font size

--- src/components/layout/Navbar/HeaderTop.tsx
+++ src/components/layout/Navbar/HeaderTop.tsx
@@ -3,11 +3,11 @@ import LanguageSelector from "./LanguageSelector";
 
 const HeaderTop = () => {
   return (
-    <div className="bg-black flex justify-center py-2 lg:py-4 text-white font-quicksand text-sm font-medium tracking-widest ">
+    <div className="bg-black flex justify-center py-2 lg:py-4 text-white font-quicksand text-sm font-medium tracking-widest">
       <div className="container flex justify-between items-center">
         <div className="flex-1" />
 
-        <span className="absolute left-1/2 -translate-x-1/2 text-center whitespace-nowrap text-xs md:text-lg">
+        <span className="absolute left-1/2 -translate-x-1/2 text-center whitespace-nowrap text-xs md:text-sm">
           RADOVANA SIMIĆA CIGE 7, BEOGRAD
         </span>
 
--- src/components/layout/Navbar/LanguageSelector.tsx
+++ src/components/layout/Navbar/LanguageSelector.tsx
@@ -7,7 +7,6 @@ import { routeMap } from "@/../lib/routeMap";
 export default function LanguageSelector() {
   const pathname = usePathname();
 
-  // proveravamo koji jezik je aktivan
   const isEnglish = pathname.startsWith("/en");
   const currentLang = isEnglish ? "en" : "sr";
   const targetLang = isEnglish ? "sr" : "en";
@@ -23,7 +22,7 @@ export default function LanguageSelector() {
     : routeMap[targetLang].home; // fallback ako ne postoji match
 
   return (
-    <Link href={targetPath} className="px-3 py-1 border rounded">
+    <Link href={targetPath} className="px-3 py-1 border rounded text-xs">
       {targetLang.toUpperCase()}
     </Link>
   );

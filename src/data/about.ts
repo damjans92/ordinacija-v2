211995955b23ffe6aa836a52e941ca021d8827d0
Damjan Savkovic <damjans92@gmail.com>
2025-11-10 23:17:32 +0100

    about text fix

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,7 +2,7 @@ export const aboutData = {
   sr: {
     aboutTitle: "Vaš stomatolog",
     aboutText:
-      "Diplomirao na Stomatološkom fakultetu Univerziteta u Beogradu 2013. godine, a 2014. nakon završenog staža otpočinje rad u  privatnoj praksi. Usavršava se u oblasti endodontologije, konzervativne stomatologije i stomatološke protetike. \n \n Paralelno sa radom učestvuje u sertifikovanim kursevima domaćih i stranih predavača iz pomenutih oblasti, kao i iz  oblasti terapije poremećaja u radu donjoviličnog zgloba i estetske medicine uz primenu hijaluronskih preparata. \n \n Tokom studija otpočinje saradnju sa Dijabetološkim savezom Srbije i učestvuje na kongresima u Berlinu i Barseloni kao stručni saradnik, gde stiče dodatna znanja iz oblasti  dijabetologije u stomatologiji. Divnu saradnju sa kolegama, domaćim i inostranim pacijentima koristi kao podstrek za osnivanje sopstvene private prakse.",
+      "Diplomirao na Stomatološkom fakultetu Univerziteta u Beogradu 2013. godine, a 2014. nakon završenog staža otpočinje rad u  privatnoj praksi. Usavršava se u oblasti endodontologije, konzervativne stomatologije i stomatološke protetike. \n \n Paralelno sa radom učestvuje u sertifikovanim kursevima domaćih i stranih predavača iz pomenutih oblasti, kao i iz  oblasti terapije poremećaja u radu donjoviličnog zgloba i estetske medicine uz primenu hijaluronskih preparata. \n \n Tokom studija otpočinje saradnju sa Dijabetološkim savezom Srbije i učestvuje na kongresima u Berlinu i Barseloni kao stručni saradnik, gde stiče dodatna znanja iz oblasti  dijabetologije u stomatologiji. Divnu saradnju sa kolegama, domaćim i inostranim pacijentima koristi kao podstrek za osnivanje sopstvene privatne prakse.",
     logoAlt: "Dr Savković Stomatološka ordinacija logo",
   },
   en: {
